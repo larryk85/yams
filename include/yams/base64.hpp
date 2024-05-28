70cc48fb56490720d272d471c69c2259330c031d
Bucky Kittinger <larrykittinger@gmail.com>
2024-05-28 19:39:25 -0400

    update

--- include/yams/base64.hpp
+++ include/yams/base64.hpp
@@ -5,11 +5,12 @@
 #include <cstring>
 
 #include <array>
+#include <span>
+#include <stdexcept>
 #include <string>
 #include <string_view>
 #include <utility>
 #include <vector>
-#include <span>
 
 
 #include "compile_time_string.hpp"
@@ -33,7 +34,7 @@ namespace yams::base64{
        * @return The index of the character in the character set, or -1 if not found.
        */
       constexpr std::size_t get_index(char c) { 
-         for (std::size_t i=0; i < 64; i++) {
+         for (std::size_t i=0; i < 256; i++) {
             if (charset[i] == c) {
                return i;
             }
@@ -48,17 +49,30 @@ namespace yams::base64{
        * @return The array of indices.
        */
       template <std::size_t... Is>
-      constexpr std::array<std::size_t, 64> generate_indices(std::index_sequence<Is...>) {
+      constexpr std::array<std::size_t, 256> generate_indices(std::index_sequence<Is...>) {
          return { detail::get_index(charset[Is])... };
       }
 
       constexpr static inline int8_t get_padding(std::span<const uint8_t> input) {
+         std::size_t sz = input.size();
+
+         constexpr std::array<uint8_t, 2> data = {'=', '='};
+
+         if (sz > 0) {
+            if (input[sz-1] == data[0]) {
+               return 1;
+            }
+            if (sz > 1) {
+               if (input[sz-2] == data[0] && input[sz-1] == data[1]) {
+                  return 2;
+               }
+            }
+         }
          switch (std::size_t sz = input.size(); sz) {
             case 0:
                return 0;
             case 1:
-               return input[1] == '=' ? 1 : 0;
-               return input[2] == '=' ? 1 : 0;
+               return input[sz-1] == '=' ? 1 : 0;
             default:
                return input[sz-1] == '=' ? 1 : 0 + (input[sz-2] == '=' ? 1 : 0);
          }
@@ -68,7 +82,7 @@ namespace yams::base64{
    /**
     * @brief The array of indices for each character in the base64 character set.
     */
-   constexpr static inline std::array<std::size_t, 64> indices = detail::generate_indices(std::make_index_sequence<64>{});
+   constexpr static inline std::array<std::size_t, 256> indices = detail::generate_indices(std::make_index_sequence<64>{});
 
    /**
     * @brief Check if a character is a valid base64 character.
@@ -122,18 +136,21 @@ namespace yams::base64{
          }
       }
 
-      if (i) {
-         std::size_t j = 0;
-         for (j=i; j < 3; j++) {
-            buffer_3[j] = '\0';
-         }
-         update(j, i+1);
-
-         while (i++ < 3) {
-            output.push_back('=');
-         }
+      std::size_t j = 0;
+      for (j=i; j < 3; j++) {
+         buffer_3[j] = '\0';
       }
-      return output;
+      update(j, i+1);
+      switch(i) {
+         default: return output;
+         case 1:
+            output.push_back('=');
+            output.push_back('=');
+            return output;
+         case 2:
+            output.push_back('=');
+            return output;
+      }
    }
 
    /**
@@ -141,56 +158,46 @@ namespace yams::base64{
     * @param input The input binary data to decode.
     * @return The decoded string.
     */
-   constexpr inline static std::string decode(const binary_t& input) {
+   inline static std::string decode(const binary_t& input) {
+      if (input.empty()) { return {}; }
+      std::size_t el = input.size() - detail::get_padding(input);
+      std::size_t dl = (el * 3) / 4;
+
       std::string output;
-      std::array<uint8_t, 4> buffer_4;
+      output.reserve(dl);
 
-      output.reserve(input.size());
+      std::size_t n = 0;
 
-      std::size_t padding = detail::get_padding(input);
-
-      const auto& update = [&](std::size_t& index, std::size_t bound) {
-         std::array<uint8_t, 3> buffer_3;
-         buffer_3[0] = (buffer_4[0] << 2) + ((buffer_4[1] & 0x30) >> 4);
-         buffer_3[1] = ((buffer_4[1] & 0x0F) << 4) + ((buffer_4[2] & 0x3C) >> 2);
-         buffer_3[2] = ((buffer_4[2] & 0x03) << 6) + buffer_4[3];
-
-         for (index=0; index < bound; index++) {
-            output.push_back(buffer_3[index]);
+      for (std::size_t i = 0, x = 0; i < input.size(); i++) {
+         if (std::isspace(static_cast<uint8_t>(input[i]))) {
+            // skip newlines
+            continue;
          }
-      };
 
-      std::size_t index = 0;
+         std::size_t d = indices[input[i]];
+         std::cout << "D: " << d << '\n';
+         std::cout << "i = " << i << '\n';
+         std::cout << "x = " << x << '\n';
+         std::cout << "input[i] = " << input[i] << '\n';
+         if (d == -1) {
+            throw std::runtime_error("Invalid base64 character");
+         }
 
-      std::size_t i = 0;
-      std::size_t l = input.size();
-      std::size_t x = 0;
-      while (l-- && (input[x] != '=') && is_base64(input[x])) {
-         buffer_4[i++] = static_cast<uint8_t>(input[x++]);
-
-         if (i == 4) {
-            for (i=0; i < 4; i++) {
-               buffer_4[i] = charset[buffer_4[i]];
+         n = (n << 6) | d;
+         if (x % 4 == 3) {
+            output.push_back((n >> 16) & 0xFF);
+            if (input[i] != '=') {
+               if (i > 0) {
+                  output.push_back((n >> 8) & 0xFF);
+               }
+               output.push_back(n & 0xFF);
             }
-
-            update(i, 3);
-            i = 0;
          }
-      }
-
-      if (i) {
-         for (std::size_t j=i; j < 4; j++) {
-            buffer_4[j] = '\0';
-         }
-
-         for (i=0; i < 4; i++) {
-            buffer_4[i] = charset[buffer_4[i]];
-         }
-
-         update(i, i-1);
+         x++;
       }
 
       return output;
    }
+
 
 } // namespace yams
